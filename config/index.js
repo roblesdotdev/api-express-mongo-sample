7488fe493b0976de283d8d2dcdd4b9b81b2d114b
roblesdotdev <httpsnip@gmail.com>
2022-09-10 19:38:47 -0300

    ensure env variable

--- config/index.js
+++ config/index.js
@@ -5,6 +5,12 @@ const configPath = path.join(__dirname, "config.env");
 
 dotenv.config({ path: configPath });
 
+function getEnv(key) {
+  if (!process.env[key]) throw new Error(`Environment ${key} must be set`);
+  return process.env[key];
+}
+
 module.exports = {
-  PORT: process.env.PORT || 5000,
+  PORT: getEnv("PORT"),
+  NODE_ENV: getEnv("NODE_ENV"),
 };
--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const http = require("http");
 const morgan = require("morgan");
-const { PORT } = require("./config");
+const { PORT, NODE_ENV } = require("./config");
 const getV1Routes = require("./routes/v1");
 
 const app = express();
 
-app.use(morgan("tiny"));
-
-app.get("/", (_req, res) => res.send("Working"));
+if (NODE_ENV !== "production") {
+  app.use(morgan("tiny"));
+}
 
 app.use("/api/v1", getV1Routes());
 
